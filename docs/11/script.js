1fbd185b6c74329f9f2a76be69a3a5d485aedcb8
211kamee <luffy020404@gmail.com>
2024-07-14 13:56:40 +0530

    ripple-on-calender

--- docs/11/script.js
+++ docs/11/script.js
@@ -9,3 +9,14 @@ monthName.innerText = today.toLocaleString("en", { month:"long" });
 dayName.innerText = today.toLocaleString("en", { weekday:"long" });
 dateName.innerText = today.getDate();
 yearName.innerText = today.getFullYear();
+
+const MouseCords = document.querySelector(`.calender`);
+
+MouseCords.addEventListener(`mouseover`, (event) => {
+    let Xcords = event.pageX - MouseCords.offsetLeft;
+    let Ycords = event.pageY - MouseCords.offsetTop;
+    MouseCords.style.setProperty(`--Xcord`, `${Xcords}px`);
+    MouseCords.style.setProperty(`--Ycord`, `${Ycords}px`);
+    Xcords = Ycords = undefined;
+})
+
